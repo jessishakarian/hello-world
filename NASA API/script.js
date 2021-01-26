40cdafc1aaf4db13815fdcf584c801b9ff8063af
spectrethree <71684035+spectrethree@users.noreply.github.com>
2021-01-26 10:57:13 -0800

    Add files via upload

---
+++ NASA API/script.js
@@ -0,0 +1,26 @@
+const config = {
+    NASA_API_KEY: 'yYfcVsWRQVcQ7ZhJ6K6B1ofhiHNm2QqnsAmP2Kzq'
+}
+
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const api_key = config.NASA_API_KEY
+
+const fetchNASAData = async () => {
+    try {
+        const response = await fetch (`${url}${api_key}`)
+        const data = await response.json()
+        console.log('NASA APOD data', data)
+        displayData(data)
+    } catch (error) {
+        console.log(error)
+    }
+ }
+
+ const displayData = data => {
+     document.getElementById('title').textContent = data.title
+     document.getElementById('date').textContent = data.date
+     document.getElementById('picture').src = data.hdurl
+     document.getElementById('explanation').textContent = data.explanation
+ }  
+
+ fetchNASAData()
